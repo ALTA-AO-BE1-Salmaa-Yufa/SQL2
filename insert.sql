9b64ec57bf81934a45ec80bd473987195c5bb63f
Salmaayufaa <salmaayufa@gmail.com>
2023-08-07 04:37:17 +0700

    done

---
+++ insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO film (film_id, film_name, Publish_date) VALUES 
+(1,'happy old year','2011-12-17'), 
+(2,'from up on poppy hill','2019-04-10'),
+(3,'a man called otto','2022-08-02'),
+(4,'ponyo','2008-01-02'),
+(5,'beef','2023-04-06'),
+(6,'forest','2020-03-01'),
+(7,'whisper of the heart','1995-03-04'),
+(8,'my neighbor totoro','1988-02-01'),
+(9,'arrietty','2010-10-10'),
+(10,'kikis delivery service','1989-01-02');
