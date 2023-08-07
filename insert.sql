0b4787b8d73dbec38d415017031c97324abaeeb2
Salmaayufaa <salmaayufa@gmail.com>
2023-08-07 17:26:38 +0700

    done

--- insert.sql
+++ insert.sql
@@ -9,3 +9,40 @@ INSERT INTO film (film_id, film_name, Publish_date) VALUES
 (8,'my neighbor totoro','1988-02-01'),
 (9,'arrietty','2010-10-10'),
 (10,'kikis delivery service','1989-01-02');
+
+/*still wrong interger value*/
+INSERT INTO `artist`(`artist_id`, `artist_name`, `artist_dob`) VALUES 
+(1,'Steven Yeun','1988-02-03'),
+(2,'Ali Wong','1989-02-09'),
+(3,'Kim Tae-ri','1981-04-10'),
+(4,'Ryu Jun Yeol','1995-01-01'),
+(5,'Tom Hanks','1960-05-07'),
+(6,'Mariana Trevino','1909-09-01'),
+(7,'Truman Hanks','1995-01-01'),
+(8,'Chutimon Chuengcharoens','1989-02-09'),
+(9,'Sunny Suwanmethanont','1981-04-10'),
+(10,'Sarika Sathsilpsupa','1988-02-03');
+
+INSERT INTO `user`(`user_id`, `name`, `password`) VALUES 
+(1,'salmaa','09183746'),
+(2,'yufa','12ljdufhu'),
+(3,'mahdiyyah','lokRTf653'),
+(4,'rizal','mksayrj'),
+(5,'salman','msknfy888'),
+(6,'ubay','ldoirybhj59'),
+(7,'dwi','lplplpljeh'),
+(8,'sonia','123456'),
+(9,'budi',',l,neyry'),
+(10,'james','hehehe1');
+
+INSERT INTO `category`(`category_id`, `category_name`) VALUES
+(1,'horror'),
+(2,'drama'),
+(3,'drama'),
+(4,'horror'),
+(5,'romance comedy'),
+(6,'comedy'),
+(7,'drama'),
+(8,'thriller'),
+(9,'horror'),
+(10,'cartoon')
